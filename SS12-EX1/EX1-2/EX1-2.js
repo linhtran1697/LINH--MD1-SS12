6ba34a02831c9f3c521605991995809c1f9942ea
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-05-25 22:52:39 +0900

    Delete SS12-EX1 directory

--- SS12-EX1/EX1-2/EX1-2.js
+++
@@ -1,7 +0,0 @@
-let tuoi = prompt ("Nhập vào số tuổi của học sinh");
-tuoi = parseInt(tuoi);
-if (tuoi <15){
-    let thongBao = "Học sinh không đủ điểu kiện vào học lớp 10"
-    alert (thongBao);
-    console.log(thongBao);
-} 
